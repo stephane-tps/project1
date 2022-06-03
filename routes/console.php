69fd5891073e62ce4b356484b09b86ac608fd81f
stephanetapasco <stephanetapascom@hotmail.com>
2022-06-03 13:22:25 +0200

    init tailwind

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('steph', function () {
+    $this->comment('Salut Steph !!!');
+})->purpose('Display hello');
+
