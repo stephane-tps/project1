543596f72c515e90b615387e66001f480aff323e
stephanetapasco <stephanetapascom@hotmail.com>
2022-06-10 10:21:31 +0200

    commit du vendredi

--- resources/views/steph/home.blade.php
+++ resources/views/steph/home.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.csss') }}" rel="stylesheet">
 
 </head>
 <body class="antialiased">
